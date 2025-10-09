c1e4c0482bf37627680a1b57f3915c07e192050a
Augustin880 <augustin.meurice@gmail.com>
2025-10-09 22:01:05 +0200

    edit

---
+++ admin/list_images.php
@@ -0,0 +1,7 @@
+<?php
+$imgDir = __DIR__ . '/../img';
+$files = array_values(array_filter(scandir($imgDir), function($f) use ($imgDir) {
+  return !is_dir("$imgDir/$f") && preg_match('/\.(png|jpe?g|gif|webp|svg)$/i', $f);
+}));
+header('Content-Type: application/json');
+echo json_encode(['images' => $files]);
